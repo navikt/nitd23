a1e392458b0779ab31a39abaf2bb45acd3689807
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2023-06-05 13:06:01 +0200

    Boilerplate for eleventy

---
+++ .eleventy.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+
+module.exports = function (eleventyConfig) {
+  const passthroughDirs = ["src/img", "src/css", "src/fonts"];
+  passthroughDirs.forEach((dir) => eleventyConfig.addPassthroughCopy(dir));
+
+  const getSvgContent = function (file) {
+    let relativeFilePath = `./src/img/${file}.svg`;
+    let data = fs.readFileSync(relativeFilePath, function (err, contents) {
+      if (err) return err;
+      return contents;
+    });
+
+    return data.toString("utf8");
+  };
+  eleventyConfig.addShortcode("svg", getSvgContent);
+
+  function formatDateHuman(date) {
+    if (!date) return "";
+    if (typeof date === "string") return formatDateHuman(new Date(date));
+
+    const monthNames = [
+      "januar",
+      "februar",
+      "mars",
+      "april",
+      "mai",
+      "juni",
+      "juli",
+      "august",
+      "september",
+      "oktober",
+      "november",
+      "desember",
+    ];
+    return `${date.getDate()}. ${
+      monthNames[date.getMonth()]
+    } ${date.getFullYear()}`;
+  }
+
+  eleventyConfig.addFilter("formatDateISO", (date) =>
+    date ? date.toISOString().substring(0, 10) : ""
+  );
+  eleventyConfig.addFilter("formatDateHuman", formatDateHuman);
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      data: "_data",
+    },
+    templateFormats: ["html", "njk", "md", "11ty.js"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    passthroughFileCopy: true,
+  };
+};
+
